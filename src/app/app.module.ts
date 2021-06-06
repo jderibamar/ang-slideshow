e9241e9d5b19c1de412fdddf938250ffaafed0c5
jderibamar <jjrrdd@gmail.com>
2021-06-05 22:41:23 -0300

    slideshow atualizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser'
 import { MDBBootstrapModule, CarouselModule, WavesModule } from 'angular-bootstrap-md'
-import { SlideshowModule } from 'ng-simple-slideshow';
+// import { SlideshowModule } from 'ng-simple-slideshow';
 
 import { AppComponent } from './app.component';
 
